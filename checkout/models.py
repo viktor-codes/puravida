1fb7432e17cbe1625ee2fd6f304a8eb975625075
Viktor Rura <ruravictor@gmail.com>
2023-11-15 21:38:47 +0000

    Minor changes in checkout, admin fields and order method

--- checkout/models.py
+++ checkout/models.py
@@ -89,4 +89,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.friendly_name} on order {self.order.order_number}'
+        return f'SKU {self.product.name} on order {self.order.order_number}'
