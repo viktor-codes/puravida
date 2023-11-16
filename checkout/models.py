cbc659cf84aa6916225389c6e40fc017b4170ac3
Viktor Rura <ruravictor@gmail.com>
2023-11-16 18:27:21 +0000

    Updated checkout models and form, added django-countries

--- checkout/models.py
+++ checkout/models.py
@@ -4,22 +4,21 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-
+from django_countries.fields import CountryField
 
 from products.models import Product
 
 
-
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    
     first_name = models.CharField(
         max_length=50, null=False, blank=False, default='Andrew')
     last_name = models.CharField(
         max_length=50, null=False, blank=False, default='Smith')
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False,default='GB')
+    country = CountryField(blank_label='Country *',
+                           null=False, blank=False, default='IE')
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -71,14 +70,17 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False,
-                              on_delete=models.CASCADE, related_name='lineitems')
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems'
+    )
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+        max_digits=6, decimal_places=2, null=False, blank=False, editable=False
+    )
 
     def save(self, *args, **kwargs):
         """
