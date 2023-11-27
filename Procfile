6dc866d4331efadffb5aafecfc634fee80ffb4ce
Viktor Rura <ruravictor@gmail.com>
2023-11-27 11:53:04 +0000

    Started deployment proccess

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn puravida.wsgi:application
--- pura_vida/settings.py
+++ pura_vida/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from env import *
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(4@zdo7(dfc(!9_@cr88&7ggj6a4%@@v9&po#@g@c+eot)im!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'puravida-b1d8a70ff589.herokuapp.com']
 
 
 # Application definition
@@ -127,12 +127,17 @@ WSGI_APPLICATION = 'pura_vida.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
