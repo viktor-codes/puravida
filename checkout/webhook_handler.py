f44170906588d69f72691a22ff29be9fa05e8c4a
Viktor Rura <ruravictor@gmail.com>
2023-11-27 19:11:36 +0000

    Updated wh handler name fields with split()

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -83,8 +83,8 @@ class StripeWH_Handler:
         while attempt <= 5:
             try:
                 order = Order.objects.get(
-                    first_name__iexact=shipping_details.first_name,
-                    last_name__iexact=shipping_details.last_name,
+                    first_name__iexact=shipping_details.name.split()[0],
+                    last_name__iexact=shipping_details.name.split()[1],
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
                     country__iexact=shipping_details.address.country,
@@ -111,8 +111,8 @@ class StripeWH_Handler:
             order = None
             try:
                 order = Order.objects.create(
-                    first_name=shipping_details.first_name,
-                    last_name=shipping_details.last_name,
+                    first_name=shipping_details.name.split()[0],
+                    last_name=shipping_details.name.split()[1],
                     user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
