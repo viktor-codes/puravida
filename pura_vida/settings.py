51922119ef0f87d8571dfbb0cf46989caaea9999
rurakite <107404981+rurakite@users.noreply.github.com>
2024-03-10 10:21:58 +0000

    returned xframe middleware

--- pura_vida/settings.py
+++ pura_vida/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = 'pura_vida.urls'
