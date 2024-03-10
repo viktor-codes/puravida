895a10f7d9f14ed78e193141c8b8619a771b55bc
rurakite <107404981+rurakite@users.noreply.github.com>
2024-03-10 10:05:03 +0000

    Removed XFrame middleware

--- pura_vida/settings.py
+++ pura_vida/settings.py
@@ -69,7 +69,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
 
